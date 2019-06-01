6fd5f2bf6bb58bf788063f0a5a15a48d4a49ec6e
bsrisompong <bsrisompong@gmail.com>
2019-06-02 00:21:36 +0700

    init

--- App.js
+++ App.js
@@ -1,28 +1,15 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 
 import Login from "./components/Login";
-import Bananas from './components/Bananas';
-import LotsOfGreeting from './components/LotsOfGreeting';
-import BlinkApp from './components/BlinkApp';
-import LotsOfStyles from './components/LotsOfStyles';
-import FixedDimensionsBasics from './components/FixedDimensionsBasics';
-import FlexDimensionsBasics from './components/FlexDimensionsBasics';
 export default class App extends React.Component {
 
   render() {
 
     return (
-      // <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      // <View >
-      //   {/* <Text>Hello, World!</Text>
-      //   <Bananas />
-      //   <LotsOfGreeting/>
-      //   <BlinkApp/>
-      //   <LotsOfStyles/> */}
-      //   {/* <FixedDimensionsBasics/> */}
-      // </View>
-        <FlexDimensionsBasics/>
+      <View style={{ paddingTop: 50 }}>
+        <Login />
+      </View>
     );
   }
 }
