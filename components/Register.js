4972b8bdb72a9ecce15e8b6f0ca3a906d2ea84f9
DESKTOP-9MGD9U7\MAY <suchanya.roekplain@gmail.com>
2019-06-04 20:54:02 +0700

    2

--- components/Register.js
+++ components/Register.js
@@ -1,24 +1,61 @@
 import React from 'react';
-import { View } from 'react-native';
+import {StyleSheet,
+  Text,
+   View,
+  TextInput,
+TouchableOpacity,
+ } from 'react-native';
 
-import Login from "./components/Login";
-export default class App extends React.Component {
-
+export default class Register extends React.Component {
   render() {
-
     return (
-        <View style={{ paddingTop: 50 }}>
-        <Text>Registration </Text>
-        <TextInput>Your Name</TextInput>
-        <TextInput>Your Email</TextInput>
-        <TextInput>Your Password</TextInput>
-        <TouchableOpacity>
-        <Text>Sign Up </Text>
+      
+        <View style={styles.regform}>
+        <Text style={styles.header}>Registration </Text>
+        <TextInput style={styles.textinput} placholder="Your Name" 
+         underlineColorAndroid={'transparent'}/>
+        <TextInput style={styles.textinput} placholder="Your Email"
+          underlineColorAndroid={'transparent'}/>
+        <TextInput style={styles.textinput} placholder="Your Password"
+        underlineColorAndroid={'transparent'}/>
+        <TouchableOpacity style={styles.button}>
+        <Text style={styles.btntext}>Sign Up </Text>
           </TouchableOpacity>
-          <Login />
         </View>
       );
     }
   }
   
-  const
+  const styles = StyleSheet.create({
+    regform: {
+      alignSelf: 'stretch',
+    },
+    header: {
+      fontSize: 24,
+      color: '#fff',
+      paddingBottom: 10,
+      marginBottom: 40,
+      borderBottomColor: '#199187',
+      borderBottomWidth: 1,
+    },
+    textinput: {
+      alignSelf: 'stretch',
+      height: 40,
+      marginBottom: 30,
+      color: '#fff',
+      borderBottomColor: '#f8f8f8',
+      borderBottomWidth: 1,
+    },
+    button: {
+      alignSelf: 'stretch',
+      alignItems: 'stretch',
+      padding: 20,
+      backgroundColor: '#59cbbd',
+      marginTop: 30,
+    },
+    btntext: {
+      color: '#fff',
+      fontWeight: 'bold',
+    }
+  });
+  
