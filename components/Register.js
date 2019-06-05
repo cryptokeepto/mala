0e0b1f837583ba7ebcef6d76f6feaa4bd08eb424
DESKTOP-9MGD9U7\MAY <suchanya.roekplain@gmail.com>
2019-06-05 17:56:41 +0700

    3

--- components/Register.js
+++ components/Register.js
@@ -1,64 +1,95 @@
 import React from 'react';
-import {StyleSheet,
+import {
+  StyleSheet,
   Text,
-   View,
+  View,
   TextInput,
-TouchableOpacity,
- } from 'react-native';
+  TouchableOpacity,
+} from 'react-native';
+import { KeyboardAvoidingView } from './C:/Users/MAY/AppData/Local/Microsoft/TypeScript/2.6/node_modules/@types/react-native';
 
 export default class Register extends React.Component {
   render() {
     return (
-
-        <View style={styles.regform}>
+<KeyboardAvoidingView behavior='padding' style={styles.wrapper}>
+      <View style={styles.container}>
         <Text style={styles.header}>Registration </Text>
-        <TextInput style={styles.textinput} placholder="Your Name" 
-         underlineColorAndroid={'transparent'}/>
-        <TextInput style={styles.textinput} placholder="Your Email"
-          underlineColorAndroid={'transparent'}/>
-        <TextInput style={styles.textinput} placholder="Your Password"
-        underlineColorAndroid={'transparent'}/>
+        <TextInput style={styles.textInput} placholder="Your Name"
+          underlineColorAndroid={'transparent'} />
+        <TextInput style={styles.textInput} placholder="Your Email"
+          underlineColorAndroid={'transparent'} />
+        <TextInput style={styles.textInput} placholder="Your Password"
+          underlineColorAndroid={'transparent'} />
         <TextInput style={styles.textinput} placholder="Comfirm Password"
-        underlineColorAndroid={'transparent'}/>
-        <TouchableOpacity style={styles.button}>
-        <Text style={styles.btntext}>Sign Up </Text>
-          </TouchableOpacity>
-        </View>
-      );
-    }
+          underlineColorAndroid={'transparent'} />
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btntext}>Sign Up </Text>
+        </TouchableOpacity>
+      </View>
+      </KeyboardAvoidingView>
+    );
   }
-  
-  const styles = StyleSheet.create({
-    regform: {
-      alignSelf: 'stretch',
-      backgroundColor: '#045792',
-    },
-    header: {
-      fontSize: 24,
-      color: '#fff',
-      paddingBottom: 10,
-      marginBottom: 40,
-      borderBottomColor: '#199187',
-      borderBottomWidth: 1,
-    },
-    textinput: {
-      alignSelf: 'stretch',
-      height: 40,
-      marginBottom: 30,
-      color: '#fff',
-      borderBottomColor: '#f8f8f8',
-      borderBottomWidth: 1,
-    },
-    button: {
-      alignSelf: 'stretch',
-      alignItems: 'stretch',
-      padding: 20,
-      backgroundColor: '#59cbbd',
-      marginTop: 30,
-    },
-    btntext: {
-      color: '#fff',
-      fontWeight: 'bold',
-    }
-  });
-  
+}
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#2896d3',
+    paddingLeft: 40,
+    paddingRight: 40,
+  },
+  header: {
+    fontSize: 24,
+    marginBottom: 60,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  textInput: {
+    alignSelf: 'stretch',
+    padding: 16,
+    marginBottom: 20,
+    backgroundColor: '#fff',
+  },
+  btn: {
+    alignSelf: 'stretch',
+    backgroundColor: '#01c853',
+    padding: 26,
+    alignItems: 'center',
+  }
+  // const styles = StyleSheet.create({
+  //   regform: {
+  //     alignSelf: 'stretch',
+  //     backgroundColor: '#045792',
+  //   },
+  //   header: {
+  //     fontSize: 24,
+  //     color: '#fff',
+  //     paddingBottom: 10,
+  //     marginBottom: 40,
+  //     borderBottomColor: '#199187',
+  //     borderBottomWidth: 1,
+  //   },
+  //   textinput: {
+  //     alignSelf: 'stretch',
+  //     height: 40,
+  //     marginBottom: 30,
+  //     color: '#fff',
+  //     borderBottomColor: '#f8f8f8',
+  //     borderBottomWidth: 1,
+  //   },
+  //   button: {
+  //     alignSelf: 'stretch',
+  //     alignItems: 'stretch',
+  //     padding: 20,
+  //     backgroundColor: '#59cbbd',
+  //     marginTop: 30,
+  //   },
+  //   btntext: {
+  //     color: '#fff',
+  //     fontWeight: 'bold',
+  //   }
+});
