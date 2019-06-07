e330cb1c2d66c55e23b1d1a23efbeea0fe42cf8c
cryptokeepto <cryptokeepto@gmail.com>
2019-06-07 17:44:20 +0700

    Add orm

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const hostname = "0.0.0.0";
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.end("index"); 
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server is running at http://${hostname}:${port}`);
+}); 
